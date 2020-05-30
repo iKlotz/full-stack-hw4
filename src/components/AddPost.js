af9a8f04c50b26400b7617f9faf5e45b65009267
iklotz <idoklotz@gmail.com>
2020-05-30 20:20:28 +0300

    db sync added

--- src/components/AddPost.js
+++ src/components/AddPost.js
@@ -1,20 +1,63 @@
-import React from "react";
+import React from 'react';
+import axios from 'axios';
 
-function AddPost () {
-    return (
-        <div className="post-form">
-            <h1>Create new post</h1>
-            <p>
-                <form>
-                    <input type="text" placeholder=" Post title goes here..." ></input>
-                    <br/><br/>
-                    <textarea rows="8" cols="50" placeholder="Post content goes here..."></textarea>
-                    <br/><br/>
-                    <button className="button" type="submit">Save post</button>
-                </form>
-            </p>
-        </div>
-    )
+
+class NewPost extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: undefined,
+            content: undefined,
+            author: undefined,
+        };
+    }
+
+    onTitleChange = (e) => {
+        this.setState({
+            title: e.target.value,
+        })
+    };
+    onContentChange = (e) => {
+        this.setState({
+            content: e.target.value,
+        })
+    };
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        const data = {
+            title: this.state.title,
+            content: this.state.content,
+            author: this.state.author,
+        };
+
+        axios.post('http://localhost:5000/posts', data).then(res => {
+            //const post = res.data;
+            this.setState({title: '',
+                content: '',
+                author:''
+            });
+        })
+    };
+
+    render() {
+        return (
+            <div className="post-form">
+                <h1>Create new post</h1>
+                <p>
+                    <form>
+                        <input type="text" placeholder=" Post title goes here..." onChange={this.onTitleChange} value={this.state.title}></input>
+                        <br/><br/>
+                        <textarea rows="8" cols="50" placeholder="Post content goes here..." onChange={this.onContentChange} value={this.state.content}></textarea>
+                        <br/><br/>
+                        <button className="button" type="submit" onClick={this.onSubmit}>Save post</button>
+                    </form>
+                </p>
+            </div>
+        )
+
+    }
 }
 
-export default AddPost;
+export default NewPost;
